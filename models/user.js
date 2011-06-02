965843d391d47cc99d6e74225cd107b72c20a759
Karl Herler <hi@karlherler.com>
2011-06-02 08:06:59 +0300

    Added users boilerplate

---
+++ models/user.js
@@ -0,0 +1,19 @@
+var user = {}
+
+function validate(req, res, next) {
+	
+}
+function signIn(req, res, next) {
+	
+}
+function create(req, res, next) {
+	
+}
+function destroy(req, res, next) {
+	
+}
+exports.validate  = validate;
+exports.signIn 		= signIn;
+exports.signOut		= signOut;
+exports.create 		= create;
+exports.destroy 	= destroy;

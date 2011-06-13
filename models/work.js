2ed6219772f66dfe53210c7b7061c8c208c86eca
Karl Herler <hi@karlherler.com>
2011-06-13 09:57:59 +0300

    forgot a damn slash

--- models/work.js
+++ models/work.js
@@ -1,7 +1,7 @@
 var  mongoose = require('mongoose');
 //data connection
 var db = (process.env.NODE_ENV==="development") ? "worktime_test" : "worktime";
-mongoose.connect('mongodb://localhost:27017'+db);
+mongoose.connect("mongodb://localhost:27017/"+db);
 
 var Schema = mongoose.Schema;
 var	ObjectId = Schema.ObjectId;

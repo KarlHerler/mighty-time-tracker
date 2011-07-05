5b5eeb1b603dd54a84921fc119fded20c527ff38
Karl Herler <hi@karlherler.com>
2011-07-05 18:16:00 +0300

    Added date labels, fixed some styling issues

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -34,16 +34,22 @@ function findUnfinished() {
 	}
 }
 
+function makeDateStr(x) {
+	var d = new Date(x); 
+	return d.getDate()+"."+(d.getMonth()+1);
+}
 function primeChart() {
 	$("#chart").hide();
   var times = [];
 	var dayTime = 0;
 	var cDay = $("tbody td").eq(0).html();
+	var labels = []
   $("tbody td:last-child").each(function() { 
 		if($(this).attr('class')!=="ongoing") {
 			if (cDay===$(this).parent().find("td").eq(0).html()) {
 				dayTime += (($(this).attr('class'))/1000/60/60);
 			} else {
+				labels.push(makeDateStr(cDay));
 				cDay = $(this).parent().find("td").eq(0).html();
 				
 				times.push(dayTime)	
@@ -52,14 +58,15 @@ function primeChart() {
 		}
 	});
 	times.push(dayTime);
+	labels.push(makeDateStr(cDay));
 	
 	times = times.reverse();
 	
   //var labels = (function lambda(x, y){ if(x>0) { y[x-1]=(x-1)/2; lambda(x-1, y); } return y })(times.length, []);
-  renderChart(times)
+  renderChart(times, labels)
 }
 
-function renderChart(times) {
+function renderChart(times, labels) {
 	$("#chart").show();
 	workChart = new Highcharts.Chart({
 		chart: {
@@ -70,6 +77,11 @@ function renderChart(times) {
 			width: 633,
 			marginLeft: 55
 	  },
+		plotOptions: {
+			areaspline: {
+				fillOpacity: .5
+			}
+		},
 	  title: {
 	  	text: ''
 	  },
@@ -78,6 +90,10 @@ function renderChart(times) {
 	    	text: ''
 	    }
 	 	},
+		xAxis: {
+			allowDecimals: true,
+			categories: labels
+		},
 	  series: [{
 	  	name: 'work',
 	    data: times
@@ -87,6 +103,11 @@ function renderChart(times) {
 		},
 		legend: {
 			enabled: false
+		}, 
+		tooltip: {
+			formatter: function() {
+				return "Hours: "+Math.round((this.y)*100)/100;
+			}
 		}
 	});
 }
